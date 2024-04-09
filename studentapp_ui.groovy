5b1b581449f16378a09765cc246e40a2c782b3c9
sonamgaikwad <sonamgaikwad1852@gmail.com>
2024-04-09 22:08:08 +0530

    --- Auto Git Commit ---

---
+++ studentapp_ui.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Pull') {
+            steps {
+                git changelog: false, poll: false, url: 'https://github.com/chetansomkuwar254/studentapp.ui'
+                echo 'Yes, Application repository pull is done !'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh '/opt/apache-maven-3.9.6/bin/mvn clean package'
+                echo 'Yes, Application Build is done !'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Here we are testing '
+            }
+        }
+        stage('Quality Test') {
+            steps {
+                echo 'Here we are testing the quality'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploy Done'
+            }
+        
+        }
+    }
+}
